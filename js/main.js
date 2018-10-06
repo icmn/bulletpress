de830952a58222dfeb0abe2d1f0dc48f62195b43
Matt Evans <mattevans404@gmail.com>
2018-10-05 20:12:41 -0500

    Algorithm tweaks, added thin spaces

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-const threshold = 10
+const cap = 755
+const margin = 10
 
 let gui={}
-gui.textArea=(()=>{
+gui.textArea = (()=>{
   let element=document.getElementById('textArea')
 
   return {
@@ -13,7 +14,7 @@ gui.textArea=(()=>{
 let textRuler = (()=>{
   let canvas = document.createElement('canvas')
   let ctx = canvas.getContext('2d')
-  ctx.font="12px Times New Roman"
+  ctx.font="12pt Times New Roman"
 
   function measure(text) {
     return Math.ceil(ctx.measureText(text).width)
@@ -22,23 +23,32 @@ let textRuler = (()=>{
   return {
     measure: measure
   }
-})
+})()
 
-let replacements = [
-  [/\s--\s/gi, '--'],
-  [/^\s/gi, '- '],
-  [/\s\/\s/gi, '/'],
-  [/\s;/gi, ';'],
-  [/\/\s/gi, '/']
-]
+gui.displayArea = (()=>{
+  let element = document.getElementById('displayArea')
+
+  return {
+    update: text => element.innerText = text
+  }
+})()
 
 let bulletPress = (string)=>{
+  
+  let replacements = [
+    [/\s*--\s*/gi, '--'],   // 'asdf -- abcd' >>> 'asdf--abcd'
+    [/^\s+/gi, '- '],       // ' asdf' >>> '- asdf'
+    [/\s+$/gi, ''],         // 'asdf ' >>> 'asdf'
+    [/\s*\/\s*/gi, '/'], 
+    [/\s*,/gi, ','],
+    [/\s+;/gi, ';'],
+    [/\s+/gi, ' ']
+  ]
+  
   let pieces = string.split('\n')
     .map(x=>x.split('|'))
 
   let bullets = ['']
-
-  console.log(pieces)
 
   pieces.forEach(terms=>{
     let tempBullets = []
@@ -50,16 +60,29 @@ let bulletPress = (string)=>{
     bullets = tempBullets.flat().slice(0)
   })
 
+  // OPTIMIZATION: Store bullets as objects with pixelLength attached
   bullets = bullets.map(x=>{
     replacements.forEach(replacement=>{
       x = x.replace(...replacement)
     })
+    return x.trim()
+  }).map(x=>{
+    while (textRuler.measure(x) > cap && x.includes(' ')) {
+      x = x.split('').reverse().join('').replace(' ','\u2006').split('').reverse().join('')
+    }
     return x
-  })
+  }).filter(x=>{
+    pixelLength = textRuler.measure(x)
+    return pixelLength <= cap && pixelLength >= cap-margin
+  }).sort((x,y)=>textRuler.measure(y)-textRuler.measure(x))
 
-  console.table(bullets)
+  console.table(bullets.map(x=>textRuler.measure(x)))
+
+  return bullets.length ? bullets : ['No bullets of length/all bullets over length']
 }
 
 gui.textArea.oninput(()=>
-  bulletPress(gui.textArea.value)
+  gui.displayArea.update(
+    bulletPress(gui.textArea.value).join('\n')
+  )
 )
